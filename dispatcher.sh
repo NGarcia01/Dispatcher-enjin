a591cd157770bdfad7eebf2824548f79334735be
NGarcia01 <Nylegarcia01@gmail.com>
2017-11-12 17:10:06 -0600

    Add files via upload

---
+++ dispatcher.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+path="./logs/latest.log"
+CURRENTEXP=""
+while true
+do
+OUT=$( tail logs/latest.log | grep "Donation" )
+echo "tail = $OUT"
+
+
+
+exp=$(echo $OUT |awk '{print $1}')
+SERVER=$(echo $OUT |awk -F'-' '{print $2}')
+Username=$(echo $OUT |awk -F'-' '{print $3}')
+TODO=$(echo $OUT |awk -F'-' '{print $4}')
+
+
+
+
+echo "$exp = $CURRENTEXP"
+ if [ "$exp" == "$CURRENTEXP" ];
+        then
+                echo found
+        else
+			echo "EXP = $exp"
+			echo "SERVER = $SERVER"
+			echo "user = $Username"
+			echo "TODO = $TODO"
+			#tmux send-keys -t $SERVER:0 "$TODO" ENTER
+			
+                CURRENTEXP=$(echo $OUT |awk '{print $1}')
+        fi
+
+sleep 1
+done

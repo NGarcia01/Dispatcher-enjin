decdc2b7944ee6165329e07b70031a0169d93714
NGarcia01 <Nylegarcia01@gmail.com>
2017-11-12 17:23:30 -0600

    Update dispatcher.sh

--- dispatcher.sh
+++ dispatcher.sh
@@ -1,10 +1,11 @@
+
 #!/bin/bash
 path="./logs/latest.log"
 CURRENTEXP=""
 while true
 do
-OUT=$( tail logs/latest.log | grep "Donation" )
-echo "tail = $OUT"
+OUT=$( tail -n 1 logs/latest.log | grep "Donation" )
+#echo "tail = $OUT"
 
 
 
@@ -16,21 +17,22 @@ TODO=$(echo $OUT |awk -F'-' '{print $4}')
 
 
 
-echo "$exp = $CURRENTEXP"
+#echo "$exp = $CURRENTEXP"
  if [ "$exp" == "$CURRENTEXP" ];
         then
                 echo found
         else
-			echo "EXP = $exp"
-			echo "SERVER = $SERVER"
-			echo "user = $Username"
-			echo "TODO = $TODO"
-			
-			#tmux send-keys -t $SERVER:0 "say Thanks for donating $Username" ENTER
-			#tmux send-keys -t $SERVER:0 "$TODO" ENTER
-			
+                        echo "EXP = $exp"
+                        echo "SERVER = $SERVER"
+                        echo "user = $Username"
+                        echo "TODO = $TODO"
+
+                        tmux send-keys -t $SERVER "say Thanks for donating $Username" ENTER
+                        tmux send-keys -t $SERVER "$TODO" ENTER
+
                 CURRENTEXP=$(echo $OUT |awk '{print $1}')
         fi
 
-sleep 1
+
 done
+

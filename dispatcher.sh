f6dcf571fb4bc2f0760526a1d6573179c75b9863
Nyle Garcia <Nylegarcia01@gmail.com>
2018-02-27 19:51:42 -0600

    updated

--- dispatcher.sh
+++ dispatcher.sh
@@ -1,38 +1,17 @@
-
-#!/bin/bash
-path="./logs/latest.log"
-CURRENTEXP=""
-while true
-do
-OUT=$( tail -n 1 logs/latest.log | grep "Donation" )
-#echo "tail = $OUT"
+while IFS= read -r OUT;
+ do
 
 
+if [[ $OUT = *"BDispatch"* ]]; then
+SERVER=$(echo $OUT  | grep "BDispatch" |awk -F '-' '{print $2}')
+TODO=$(echo $OUT  | grep "BDispatch" |awk -F '-' '{print $3}')
 
-exp=$(echo $OUT |awk '{print $1}')
-SERVER=$(echo $OUT |awk -F'-' '{print $2}')
-Username=$(echo $OUT |awk -F'-' '{print $3}')
-TODO=$(echo $OUT |awk -F'-' '{print $4}')
+echo $SERVER - $TODO
 
-
-
-
-#echo "$exp = $CURRENTEXP"
- if [ "$exp" == "$CURRENTEXP" ];
-        then
-                echo found
-        else
-                        echo "EXP = $exp"
-                        echo "SERVER = $SERVER"
-                        echo "user = $Username"
-                        echo "TODO = $TODO"
-
-                        tmux send-keys -t $SERVER "say Thanks for donating $Username" ENTER
-                        tmux send-keys -t $SERVER "$TODO" ENTER
-
-                CURRENTEXP=$(echo $OUT |awk '{print $1}')
-        fi
+tmux send-keys -t $SERVER "$TODO" ENTER
 
 
-done
+fi
 
+
+ done < <(tail -f -n 1 logs/latest.log )

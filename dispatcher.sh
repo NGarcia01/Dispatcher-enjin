1cb13be3796bc0149069bf9db1997fea03a07994
Nyle Garcia <6970306+NyleGarcia-dev@users.noreply.github.com>
2018-07-21 11:41:11 -0500

    Add files via upload

--- dispatcher.sh
+++ dispatcher.sh
@@ -1,17 +1,58 @@
-while IFS= read -r OUT;
- do
 
+_tmux_send_keys_all_panes_ () {
+  for _pane in $(tmux list-panes -F '#P'); do
+    tmux send-keys -t ${_pane} "$@"
+  done
+}
+
+
+while IFS= read -r OUT;
+do
+
+
+echo $OUT | grep "BDispatch"
 
 if [[ $OUT = *"BDispatch"* ]]; then
-SERVER=$(echo $OUT  | grep "BDispatch" |awk -F '-' '{print $2}')
-TODO=$(echo $OUT  | grep "BDispatch" |awk -F '-' '{print $3}')
 
-echo $SERVER - $TODO
 
-tmux send-keys -t $SERVER "$TODO" ENTER
+
+SERVER=$(echo $OUT  | grep "BDispatch"|awk -F '-' '{print $2}') 
+Test="$(echo $OUT  | grep "BDispatch"|awk -F '-' '{print $3}')"
+ 
+
+echo $SERVER - $Test
+
+
+if [[ $SERVER = *"ALL"* ]]; then
+
+echo all
+
+for _pane in $(tmux list-panes -a -F '#{pane_id}'); do \
+
+
+echo ${_pane}
+  tmux send-keys -t ${_pane} "$Test" ENTER 
+
+ done
+
+else 
+if [[ $OUT = *"-/"* ]]; then
+
+Test="$(echo $OUT  | grep "BDispatch"|awk -F '-/' '{print $2}')"
+
 
 
 fi
 
 
- done < <(tail -f -n 1 logs/latest.log )
+tmux send-keys -t $SERVER "$Test" ENTER
+
+fi
+
+fi
+
+LAST=$Test
+
+ done < <( tail -f -n 0 --follow=name --retry logs/*.log )
+
+
